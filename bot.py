f0b74682dfa29f0757b7613fa9554c7529c19bd3
DeflatedPickle <deflatedpickle@gmail.com>
2021-11-29 00:43:53 +0000

    Added an embed for edited messages

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import json
 from json import JSONDecodeError
 
-from discord import Intents
+from discord import Intents, Activity, ActivityType
 from discord.ext.commands import Bot
 from discord_slash import SlashCommand
 
 from cogs.guild import CogGuild
 from cogs.message import CogMessage
+from cogs.slash import CogSlash
 from cogs.user import CogUser
 
 with open("token.txt") as f:
@@ -27,8 +28,19 @@ bot = Bot(
 )
 slash = SlashCommand(bot)
 
+
+@bot.event
+async def on_ready():
+    print(f"1984-chan is connected to {len(bot.guilds)} guilds")
+    print("this is literally 1984")
+
+    await bot.change_presence(activity=Activity(type=ActivityType.watching, name="you"))
+
+
 bot.add_cog(CogMessage(bot, config))
 bot.add_cog(CogGuild(bot, config))
 bot.add_cog(CogUser(bot, config))
+
+bot.add_cog(CogSlash(bot, config))
 
 bot.run(botid)
--- cogs/message.py
+++ cogs/message.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from discord import Message, Embed, RawMessageUpdateEvent
+from discord import Message, Embed
 from discord.ext.commands import Cog
 
 
@@ -36,5 +36,36 @@ class CogMessage(Cog):
 
     # we're using the raw one as the message could be too old to be in the cache
     @Cog.listener()
-    async def on_raw_message_edit(self, payload: RawMessageUpdateEvent):
+    async def on_message_edit(self, before: Message, after: Message):
+        if before.content == after.content: return
+
         await self.bot.wait_until_ready()
+
+        try:
+            c = self.config[before.author.id]["channel"]
+        except KeyError:
+            c = before.channel.id
+
+        embed = Embed(
+            timestamp=datetime.now(),
+        ) \
+            .set_author(
+            name=before.author.name,
+            icon_url=before.author.avatar_url,
+        ) \
+            .set_footer(
+            text="(edited)"
+        ) \
+            .add_field(
+            name="before",
+            value=before.content,
+            inline=False,
+        ) \
+            .add_field(
+            name="after",
+            value=after.content,
+            inline=False,
+        )
+
+        channel = self.bot.get_channel(c)
+        await channel.send(embed=embed)
